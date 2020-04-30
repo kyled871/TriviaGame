077ae732a9eea634c31648da36bf4b57e2e79e02
Kyle Davis <kyled871@yahoo.com>
2020-04-30 16:26:57 -0400

    added questions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() { 
+
+    let correct = 0
+    let incorrect = 0
+
+    const gameQuestions = [
+        {
+            question: "What does Hank sell for a living?",
+            choices: ['propane and propane accessories', 'beer', 'pest control products', 'cars'],
+            answer: 'propane and propane accessories',
+        },
+
+        {
+            question: "What is the name of Hank's son?",
+            choices: ['Billy', 'Bobby', 'Chuck', 'Buckley'],
+            answer: 'Bobby',
+        },
+
+        {
+            question: "Kahn is Hank's neighbor. What is the name of their daughter?",
+            choices: ['Kahn Souphanousinphone Jr', 'Dale', 'Connie', 'Lou Anne'],
+            answer: 'Kahn Souphanousinphone Jr',
+        },
+    ]
+
+    
+
+});
